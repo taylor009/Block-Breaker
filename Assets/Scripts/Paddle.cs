1333d91e1a013c75ca2e1186d3df4dc861502c3d
Taylor Gagne <taylor@baremetalcloud.com>
2019-04-26 23:37:15 -0400

    Added paddle movement constraints
    Created small method to limit paddle movement to be inside of the camera view.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    // Configuration paramaters
+
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +21,8 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePauseInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePauseInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePauseInUnits, minX, maxX);
         transform.position = paddlePos;
     }
 }
