ec518197271be8f734dc2e3494661c25a48a0182
Taylor Gagne <taylor@baremetalcloud.com>
2019-04-27 16:18:00 -0400

    Created Block counter

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,15 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    //cached refence
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.countBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] int breakableBlocks;  //Serialize for debugging purpose.
+
+    public void countBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+}
